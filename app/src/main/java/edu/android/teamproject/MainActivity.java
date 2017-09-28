ac29eef9315dacde1a7e1432fbb39eefe68c58fc
HanTaeJun <ac85kr@gmail.com>
2017-09-28 09:44:54 +0900

    한태준 +TODO 추가. 확인요망

--- app/src/main/java/edu/android/teamproject/MainActivity.java
+++ app/src/main/java/edu/android/teamproject/MainActivity.java
@@ -293,6 +293,8 @@ public class MainActivity extends AppCompatActivity
     //TODO: 2. 온터치시 안보이는게아닌 클릭시 안보이게 하는 기능으로 변환
     //TODO: 3. 카메라 가로/세로 를 구분하는 기능 필요.
     //TODO: 4. 텍스트입력시 Text Font 및 color 를 변경할수 있게 하는 기능 추가
-    //TODO: 5. 기능 구현후 UI 보완
+    //TODO: 5. 현재 activity_main.xml 에 있는 '텍스트 입력' 버튼을 플로팅 버튼형식으로 변경
+    //TODO: 6. 기능 구현후 UI 보완
+
 
 }
